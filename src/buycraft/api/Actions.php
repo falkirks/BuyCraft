81d9cfce33af8fbdb684a68ed813ebcb0940df01
Falkirks <bpnoreply@gmail.com>
2014-09-05 09:22:35 -0700

    Added remaining Action constants

--- src/buycraft/api/Actions.php
+++ src/buycraft/api/Actions.php
@@ -6,4 +6,12 @@ class Actions{
     const PLUGIN_VERSION = "6.7";
 
     const AUTHENTICATE = "info";
+    const CATEGORIES = "categories";
+    const URL = "url";
+    const PACKAGES = "packages";
+    const PAYMENTS = "payments";
+    const PENDING_PLAYERS = "pendingUsers";
+    const COMMANDS = "commands";
+        const LOOKUP = "lookup";
+        const REMOVE = "removeId";
 }
